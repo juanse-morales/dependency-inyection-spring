34e3c7ee110372c68dd5d9bca153eae5191a692f
Juan Morales <jmorales@dcatech.com>
2020-11-13 20:22:48 -0500

    Video 22: Evaluación de expresiones con SpEL

--- src/main/java/com/dca/di/DependencyInyectionApplication.java
+++ src/main/java/com/dca/di/DependencyInyectionApplication.java
@@ -2,12 +2,11 @@ package com.dca.di;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-
-import com.dca.di.autowire.AreaCalculatorService;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 
 @SpringBootApplication
 public class DependencyInyectionApplication {
@@ -21,13 +20,9 @@ public class DependencyInyectionApplication {
 	}
 	
 	public static void main(String[] args) {
-		ConfigurableApplicationContext context = SpringApplication.run(DependencyInyectionApplication.class, args);
-		
-		AreaCalculatorService calculator = context.getBean(AreaCalculatorService.class);
-		
-		log.info("Area total: {} ", calculator.calcAreas());
-		
-		context.close();
+		ExpressionParser parser = new SpelExpressionParser();
+		Expression expression = parser.parseExpression("10 || 20");
+		log.info("Result: {} ", expression.getValue());
 	}
 
 }
