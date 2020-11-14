f357144a037886761603cc5bbadecd16ff6ac451
Juan Morales <jmorales@dcatech.com>
2020-11-14 15:49:08 -0500

    Video 25: Callbacks @PostConstruct y @PreDestroy

--- src/main/java/com/dca/di/DependencyInyectionApplication.java
+++ src/main/java/com/dca/di/DependencyInyectionApplication.java
@@ -2,11 +2,12 @@ package com.dca.di;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.expression.Expression;
-import org.springframework.expression.ExpressionParser;
-import org.springframework.expression.spel.standard.SpelExpressionParser;
+
+import com.dca.di.lifecycle.LifeCycleBean;
 
 @SpringBootApplication
 public class DependencyInyectionApplication {
@@ -20,9 +21,9 @@ public class DependencyInyectionApplication {
 	}
 	
 	public static void main(String[] args) {
-		ExpressionParser parser = new SpelExpressionParser();
-		Expression expression = parser.parseExpression("10 || 20");
-		log.info("Result: {} ", expression.getValue());
+		ConfigurableApplicationContext context = SpringApplication.run(DependencyInyectionApplication.class, args);
+		LifeCycleBean bean = context.getBean(LifeCycleBean.class);
+		
 	}
 
 }
