f7638e8954b93b76ff3daf50d696de6193c444f1
Juan Morales <jmorales@dcatech.com>
2020-11-14 17:56:08 -0500

    Video 29: BeanPostProcessor

--- src/main/java/com/dca/di/DependencyInyectionApplication.java
+++ src/main/java/com/dca/di/DependencyInyectionApplication.java
@@ -13,24 +13,8 @@ import com.dca.di.lifecycle.LifeCycleBean;
 @SpringBootApplication
 public class DependencyInyectionApplication {
 
-	
-	private static final Logger log = LoggerFactory.getLogger(DependencyInyectionApplication.class);
-
-	
-	/* @Bean
-	public String getApplicationName() {
-		return "DCA";
-	} */
-	
-	/* @Bean(initMethod = "init", destroyMethod = "destroy")
-	public ExplicitBean getBean() {
-		return new ExplicitBean();
-	} */
-	
 	public static void main(String[] args) {
 		ConfigurableApplicationContext context = SpringApplication.run(DependencyInyectionApplication.class, args);
-		//LifeCycleBean bean = context.getBean(LifeCycleBean.class);
-		
 	}
 
 }
---
+++ src/main/java/com/dca/di/lifecycle/DCABeanPostProcessor.java
@@ -0,0 +1,26 @@
+package com.dca.di.lifecycle;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+@Component
+public class DCABeanPostProcessor implements BeanPostProcessor {
+
+	private static final Logger log = LoggerFactory.getLogger(DCABeanPostProcessor.class);
+	
+	@Override
+	public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
+		log.info("Before initialization {} ", beanName);
+		return bean;
+	}
+
+	@Override
+	public Object postProcessAfterInitialization(Object bean, String beanName) throws BeansException {
+		log.info("After initialization {} ", beanName);
+		return bean;
+	}
+	
+}
